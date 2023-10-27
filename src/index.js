1ab4300bb9f19b38daea41efed9d63c638d00f42
Sychov-Yurii <sychov.yurii@outlook.com>
2023-10-27 20:46:49 +0200

    update

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const elements = {
+    form: document.querySelector('.search-form'),
+    searchInput: document.querySelector('input[name="searchQuery"]'),
+};
+
+elements.form.addEventListener('submit', onSearchCats);
+
+async function onSearchCats(evt) {
+    evt.preventDefault()
+    const formData = new FormData (evt.currentTarget);
+    const cats = formData.getAll('searchQuery');
+    await serviceGetCats(cats);
+};
+
+async function serviceGetCats(arr) {
+    const API_KEY = '40315938-f4b95c5c15917d8c0c53825d3';
+    const promises = arr.map(async cat => {
+        try {
+            const resp = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${cat}&image_type=photo&orientation=horizontal&safesearch=true`);
+            if (resp.ok) {
+                const data = await resp.json();
+                console.log(data);
+            } else {
+                console.error('Error fetching data: ', resp.status);
+            }
+        } catch (error) {
+            console.error('An error occured: ', error);
+        }
+ });
+ const data = await Promise.allSettled(promises);
+ const cats = data.filter(({status}) => status === 'fulfilled').map(({value}) => )
+ console.log(cats);
+}
