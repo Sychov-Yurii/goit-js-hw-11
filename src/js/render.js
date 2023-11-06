94bb457625c245a85a768fb90139efb5fb1ef36b
Sychov-Yurii <sychov.yurii@outlook.com>
2023-11-06 09:53:42 +0100

    update

--- src/js/render.js
+++ src/js/render.js
@@ -17,113 +17,253 @@ let search = '';
 let lightbox;
 const notifyOptions = { position: 'center-center', timeout: 10000 };
 
-export function onSearch(evt) {
-       
+
+export async function getPhoto(search, page, perPage) {
+    try {
+        const response = await fetch(`${url}?key=${api}&q=${search}&page=${page}&per_page=${perPage}`);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data from the server. Status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw error; // Re-throw the error to be handled in the calling code
+    }
+}
+
+export async function onSearch(evt) {
     evt.preventDefault(); // Prevent form submission
-    loadMore.classList.add('is-hidden')
-    search = evt.currentTarget.searchQuery.value.trim();//get search value without spaces
-    
-    console.log(search)
-    
-    gallery.innerHTML = '';//clear gallery
-    
+    loadMore.classList.add('is-hidden');
+    search = evt.currentTarget.searchQuery.value.trim(); // get search value without spaces;
+
+    console.log(search);
+
+    gallery.innerHTML = ''; // clear gallery;
+
     page = 1;
-    
 
     if (search === '') {
-        Notify.info('What are you looking for?')
-        alert('!')
-        return
+        Notify.info('What are you looking for?');
+        return;
     }
-       
-    getPhoto(search, page, perPage)
-        
-        .then(data => {
-            console.log(data.totalHits, data.hits)
-            loadMore.classList.add('is-hidden')
-            if (data.totalHits === 0) {
-                nothing()
-        }
-            else {
-                
-                createMurkup(data.hits);
-                lightbox = new simpleLightbox('.gallery a', { 
+
+    try {
+        const data = await getPhoto(search, page, perPage);
+        console.log(data.totalHits, data.hits);
+        loadMore.classList.add('is-hidden');
+        if (data.totalHits === 0) {
+            nothing();
+        } else {
+            createMurkup(data.hits);
+            lightbox = new simpleLightbox('.gallery a', {
                 captions: true,
                 captionSelector: 'img',
                 captionsData: 'alt',
                 captionDelay: '250',
                 alertErrorMessage: '（╯‵□′）╯︵┴─┴',
-                 overlay: true,
+                overlay: true,
                 overlayOpacity: 0.4,
-                navText: ['←','→'],
-                    }).refresh();
-                    scroll()
-            }
+                navText: ['←', '→'],
+            }).refresh();
+            scroll();
+        }
 
-            //load more page
-            
-            if (data.totalHits > perPage) {
-                loadMore.classList.remove('is-hidden')
-                
-            }
-    })
-     
+        if (data.totalHits > perPage) {
+            loadMore.classList.remove('is-hidden');
+        }
+    } catch (error) {
+        console.error(error);
+        Notify.failure("An error occurred while fetching images.");
+        loadMore.classList.add('is-hidden');
+    }
+}
+
+export async function addPages() {
+    page++;
+    try {
+        const data = await getPhoto(search, page, perPage);
+        console.log(data.totalHits, data.hits);
+        console.log(Math.ceil(data.totalHits / perPage));
+
+        console.log(page);
+        if (Math.ceil(data.totalHits / perPage) === page) {
+            Notify.info(`We're sorry, but you've reached the end of search results.`);
+            loadMore.classList.add('is-hidden');
+        }
+
+        createMurkup(data.hits);
+        lightbox = new simpleLightbox('.gallery a', {
+            captions: true,
+            captionSelector: 'img',
+            captionsData: 'alt',
+            captionDelay: '250',
+            alertErrorMessage: '（╯‵□′）╯︵┴─┴',
+            overlay: true,
+            overlayOpacity: 0.4,
+            navText: ['←', '→'],
+        }).refresh();
+        scroll();
+    } catch (error) {
+        console.error(error);
+        Notify.failure("An error occurred while fetching images.");
+        loadMore.classList.add('is-hidden');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export function onSearch(evt) {
+       
+//     evt.preventDefault(); // Prevent form submission
+//     loadMore.classList.add('is-hidden')
+//     search = evt.currentTarget.searchQuery.value.trim();//get search value without spaces
+    
+//     console.log(search)
+    
+//     gallery.innerHTML = '';//clear gallery
+    
+//     page = 1;
+    
+
+//     if (search === '') {
+//         Notify.info('What are you looking for?')
+//         // alert('!')
+//         return
+//     }
+       
+//     getPhoto(search, page, perPage)
         
+//         .then(data => {
+//             console.log(data.totalHits, data.hits)
+//             loadMore.classList.add('is-hidden')
+//             if (data.totalHits === 0) {
+//                 nothing()
+//         }
+//             else {
+                
+//                 createMurkup(data.hits);
+//                 lightbox = new simpleLightbox('.gallery a', { 
+//                 captions: true,
+//                 captionSelector: 'img',
+//                 captionsData: 'alt',
+//                 captionDelay: '250',
+//                 alertErrorMessage: '（╯‵□′）╯︵┴─┴',
+//                  overlay: true,
+//                 overlayOpacity: 0.4,
+//                 navText: ['←','→'],
+//                     }).refresh();
+//                     scroll()
+//             }
+
+//             //load more page
+            
+//             if (data.totalHits > perPage) {
+//                 loadMore.classList.remove('is-hidden')
+                
+//             }
+//     })
+     
+//         .catch(error => {
+//             console.error(error);
+//             // Handle the error here, such as displaying a user-friendly error message.
+//             Notify.failure("An error occurred while fetching images.");
+//             loadMore.classList.add('is-hidden')
+//         })
+// }
     
-        .catch(error => {
-            console.error(error);
-            // Handle the error here, such as displaying a user-friendly error message.
-            Notify.failure("An error occurred while fetching images.");
-            loadMore.classList.add('is-hidden')
-        })
-}
+// // export async function getPhoto(search, page, perPage) {
+// //     try {
+// //         const response = await fetch(`${url}?key=${api}&q=${search}&page=${page}&per_page=${perPage}`);
+// //         if (!response.ok) {
+// //             throw new Error(`Failed to fetch data from the server. Status: ${response.status}`);
+// //         }
+// //         const data = await response.json();
+// //         return data;
+// //     } catch (error) {
+// //         throw error; // Re-throw the error to be handled in the calling code
+// //     }
+// // }
+
+// export async function addPages() {
+//     page++
+//     getPhoto(search, page, perPage)
+//         .then(data => {
+//             console.log(data.totalHits, data.hits)
+//             console.log(Math.ceil(data.totalHits / perPage))
+            
+//             console.log(page)
+//             if (Math.ceil(data.totalHits/perPage) === page) {
+//                 Notify.info(`We're sorry, but you've reached the end of search results.`)
+//                 loadMore.classList.add('is-hidden')
+//             }
+            
+//                 createMurkup(data.hits);
+//                 lightbox =new simpleLightbox('.gallery a', {
+//                     captions: true,
+//                     captionSelector: 'img',
+//                     captionsData: 'alt',
+//                     captionDelay: '250',
+//                     alertErrorMessage: '（╯‵□′）╯︵┴─┴',
+//                     overlay: true,
+//                     overlayOpacity: 0.4,
+//                     navText: ['←', '→'],
+//                 }).refresh();
+//                 scroll()
+            
+//         })
+//      .catch(error => {
+//             console.error(error);
+//             // Handle the error here, such as displaying a user-friendly error message.
+//          Notify.failure("An error occurred while fetching images.");
+//          loadMore.classList.add('is-hidden')
+//         })
+// }
+
+
+
+// function nothing() {
+//     Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+//     loadMore.classList.add('is-hidden')
+// }
+
+// function scroll() {
+//     const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
     
-export function addPages() {
-    page++
-    getPhoto(search, page, perPage)
-        .then(data => {
-            console.log(data.totalHits, data.hits)
-            console.log(Math.ceil(data.totalHits / perPage))
-            
-            console.log(page)
-            if (Math.ceil(data.totalHits/perPage) === page) {
-                Notify.info(`We're sorry, but you've reached the end of search results.`)
-                loadMore.classList.add('is-hidden')
-            }
-            
-                createMurkup(data.hits);
-                lightbox =new simpleLightbox('.gallery a', {
-                    captions: true,
-                    captionSelector: 'img',
-                    captionsData: 'alt',
-                    captionDelay: '250',
-                    alertErrorMessage: '（╯‵□′）╯︵┴─┴',
-                    overlay: true,
-                    overlayOpacity: 0.4,
-                    navText: ['←', '→'],
-                }).refresh();
-                scroll()
-            
-        })
-     .catch(error => {
-            console.error(error);
-            // Handle the error here, such as displaying a user-friendly error message.
-         Notify.failure("An error occurred while fetching images.");
-         loadMore.classList.add('is-hidden')
-        })
-}
+//     // Add a conditional check to determine whether scrolling should occur
+//     if (cardHeight * 2 > window.innerHeight) {
+//         window.scrollBy({
+//             top: cardHeight * 2,
+//             behavior: "smooth",
+//         });
+//     }
+// }
 
 
-function nothing() {
-    Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-    loadMore.classList.add('is-hidden')
-}
 
-function scroll() {
-    const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
-
-window.scrollBy({
-  top: cardHeight * 2,
-  behavior: "smooth",
-});
-}
